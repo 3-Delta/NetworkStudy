4bda79b1770232f963b9b6e23d003fde50e3cf1d
kaclok <572005238@qq.com>
2019-01-06 15:15:42 +0800

    unity在线程中调用unity的class 比如texture等会出现错误

--- Client/Assets/Scripts/Logic/UI/UI_Main.cs
+++ Client/Assets/Scripts/Logic/UI/UI_Main.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,12 +10,32 @@ public class UI_Main : MonoBehaviour
     public Button btnMail;
     public Button btnLogin;
 
+    private Thread thread;
+
     private void Awake()
     {
         btnLunch.onClick.AddListener(OnBtnLunchClicked);
         btnMail.onClick.AddListener(OnBtnMailClicked);
         btnLogin.onClick.AddListener(OnBtnLoginClicked);
+
+        thread = new System.Threading.Thread(new System.Threading.ThreadStart(Log));
+        thread.Start();
     }
+    private void Log()
+    {
+        while (true)
+        {
+            Thread.Sleep(200);
+            // 线程中使用unity的class[非struct], 会导致报错：get_gameObject can only be called from the main thread.
+            btnMail.interactable = false;
+            Debug.LogError("--------");
+        }
+    }
+    private void OnDestroy()
+    {
+        thread?.Abort();
+    }
+
     private void OnBtnLunchClicked()
     {
         NW_Mgr.Instance.Connect(NW_Def.IPv4, NW_Def.PORT);
